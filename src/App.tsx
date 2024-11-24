6bc16b426c17900b29481eb79af7f108a420eb7b
sli39 <sliva391985@gmail.com>
2024-11-24 16:48:07 +0300

    comit 1

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          куку ептааааааааааааааааааа
+          куку ептawdawdawdawdааааааааааааааааааа
         </p>
+        <p>waawdaw awdddadwww</p>
         <a
           className="App-link"
           href="https://reactjs.org"
